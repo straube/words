bd4eba95fe48321e17df8a089edbfc23e874cf47
Gustavo Straube <gustavo@creativeduo.com.br>
2019-10-30 22:01:12 -0300

    Improved speech tone.

--- script.js
+++ script.js
@@ -81,8 +81,8 @@
     function sayWord(word) {
         const utter = new SpeechSynthesisUtterance(word);
         utter.voice = voice;
-        utter.pitch = 0.9;
-        utter.rate = 0.7;
+        utter.pitch = 0.8;
+        utter.rate = 0.6;
         synth.speak(utter);
     }
 
